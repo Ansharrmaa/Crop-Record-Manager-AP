9ec67aa30b9dfd3b7af5da1edfc14e3f0394d6f1
Anshsharrmaa <ansh87624@gmail.com>
2025-08-08 00:29:45 +0530

    Error handling middleware

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ app.use('/api/crops', cropRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
